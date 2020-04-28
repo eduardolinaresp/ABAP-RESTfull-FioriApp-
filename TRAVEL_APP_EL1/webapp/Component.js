0f6aecbeffc6aa2f2ce0053abea2d723d6b470b3
Eduardo Linares <eduardolinaresp@gmail.com>
2020-04-28 06:22:28 +0000

    primer commit

---
+++ TRAVEL_APP_EL1/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("namespace_el1.travel_app_el1.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
